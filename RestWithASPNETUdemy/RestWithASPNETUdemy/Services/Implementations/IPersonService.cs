e63ffb1634d74addcbee05be2eb197c53040feba
Appito <fernanda@appito.com>
2023-07-03 15:13:00 -0300

    encerrando seção 5 - nivelamento

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll(); // retorna uma lista com todas as pessoas
+        Person Update(Person person);
+        Person Delete(long id);
+
+    }
+}
