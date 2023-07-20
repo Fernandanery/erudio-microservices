e7cbc07cdbfdaee1cd670e33cc1cd29de37a08d4
Appito <fernanda@appito.com>
2023-07-20 20:47:02 -0300

    iniciando migrations

---
+++ S07_ErudioMicroservices.NET6-FirstMicroservices/GeekShopping/GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,35 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model
+{
+    [Table("Product")] //nome da tabela
+    public class Product : BaseEntity
+    {
+        //Colunas da tabela
+
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("Price")]
+        [Required]
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
+
+        [Column("Description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("CategoryName")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("ImageURL")]
+        [StringLength(300)]
+        public string ImageURL { get; set; }
+
+    }
+}
--- S07_ErudioMicroservices.NET6-FirstMicroservices/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S07_ErudioMicroservices.NET6-FirstMicroservices/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,16 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MySqlConnector;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0))));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
