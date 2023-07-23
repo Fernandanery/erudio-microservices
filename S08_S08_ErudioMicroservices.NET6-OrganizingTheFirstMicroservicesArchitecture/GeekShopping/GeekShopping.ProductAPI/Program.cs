2adbcb81dc775ff85e82d3914e6eb5a094bdd10a
Appito <fernanda@appito.com>
2023-07-23 18:27:33 -0300

    [S8] - implementações e iniciando controller

--- S08_S08_ErudioMicroservices.NET6-OrganizingTheFirstMicroservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_S08_ErudioMicroservices.NET6-OrganizingTheFirstMicroservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,20 +1,31 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Configurando conexão do banco de dados
 
 var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+builder.Services.AddDbContext<MySqlContext>(options => 
+                                            options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0))));
 
-builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0))));
+// Configurando o AutoMapper
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Injetando o productRepository
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen( c =>
